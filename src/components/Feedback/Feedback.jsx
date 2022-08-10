de8a719280e39a0d75d3a1f6f04a546c7c1ceae9
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-08-10 15:21:28 +0300

    first task

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 export const Feedback = () => {
   const [good, setGood] = useState(0);
-  const [neutral, setneutral] = useState(0);
+  const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const countTotalFeedbacks = () => {
@@ -21,14 +21,29 @@ export const Feedback = () => {
   //   }));
   // };
 
-  const handleGood = () => {
-    setGood(good + 1);
+  const handleChange = bntName => {
+    switch (bntName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
   const onClearBtn = () => {
     setGood(0);
     setBad(0);
-    setneutral(0);
+    setNeutral(0);
   };
 
   const options = ['good', 'bad', 'neutral'];
@@ -39,7 +54,7 @@ export const Feedback = () => {
       <Section title="Please leave feedback">
         <FeedbackOptions
           options={options}
-          onLeaveFeedback={handleGood}
+          onLeaveFeedback={handleChange}
           onClear={onClearBtn}
         ></FeedbackOptions>
       </Section>
